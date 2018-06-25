984f14d3bb0f4c1947376c54a59d9c2174ab9f70
Munachimso Ugorji <munachimsougorji@Munachimsos-MacBook-Pro-2.local>
2018-06-25 19:57:08 +0100

    basic home page functional

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,17 +1,17 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import Header from '../Common/Header';
 
 class HomePage extends Component {
 
-    render() {
-        return (
-            <View>
-                <Header />
-                <Text>This is Login Page for now</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+        <View>
+            <Header />
+            <Text>I'm here</Text>
+        </View>
+    );  
+  }
 }
 
 export default HomePage;
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import Splash from './Splash';
 import Login from './Components/Login/Login';
 import HomePage from './Components/Home/HomePage';
@@ -8,12 +8,12 @@ const IntroStack = StackNavigator({
     splash:     { screen: Splash },
     login:      { screen: Login },
     home:       { screen: HomePage },
-
      }, 
     { headerMode: 'none' })
 
-const PrimaryNavigation = StackNavigator(
-    { IntroStack: { screen: IntroStack }},
+const Navigation = StackNavigator({ 
+    IntroStack: { screen: IntroStack }
+},
     { headerMode: 'none' })
 
-export default PrimaryNavigation;
+export default Navigation;
