0bd4e710d340b22c4c87b966442d8c5794a8da2d
marquis-d-ugo <munachimso.ugorji@andela.com>
2018-07-03 18:00:06 +0100

    done with first batch of frontend

--- src/Components/Report/ReportPage.js
+++ src/Components/Report/ReportPage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text, Linking, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
+import { Form, Picker, Icon } from 'native-base';
 import Header from '../Common/Header';
 import SafetyFirst from '../Common/SafetyFirst';
 import Button from '../Common/Button1'
 
 class ReportPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected2: undefined
+    };
+  }
+  onValueChange2(value: string) {
+    this.setState({
+      selected2: value
+    });
+  }
 
   onSuccess(e) {
     this.props.navigation.navigate('submit')
@@ -17,24 +30,36 @@ class ReportPage extends Component {
         <View style={{ flex: 1}}>
             <Header style={{ flex: 1}}/>
             <SafetyFirst style={{ flex: 1}}/>
-            <View style={{ marginTop: 0, flex: 1 }}>
-              <QRCodeScanner
-                onRead={this.onSuccess.bind(this)}
-                topContent={
-                  <Text style={styles.centerText}>
-                    Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-                  </Text>
-                }
-                bottomContent={
-                  <TouchableOpacity style={styles.buttonTouchable}>
-                    <Text style={styles.buttonText}>OK. Got it!</Text>
-                  </TouchableOpacity>
-                }
-              />
-            </View>
-            <Button style={{ flex: 1}} onPress={() => this.props.navigation.navigate('submit')}>
-                    GO TO SUBMIT
-            </Button>
+            <ScrollView>
+              <View style={{ padding: 5, flex: 1 }}>
+                <Text style={{ fontSize: 20, fontWeight: '700', marginTop: 10 }}> SCAN THE QR CODE</Text>
+                <QRCodeScanner
+                  onRead={this.onSuccess.bind(this)}
+                />
+              </View>
+              <Form>
+              <Text style={{ fontSize: 20, fontWeight: '700', marginTop: 10 }}> OR Select desired location below</Text>
+                <Picker
+                  mode="dropdown"
+                  iosIcon={<Icon name="ios-arrow-down-outline" />}
+                  placeholder="Select Incident Location"
+                  placeholderStyle={{ color: "#bfc6ea" }}
+                  placeholderIconColor="#007aff"
+                  style={{ width: undefined }}
+                  selectedValue={this.state.selected2}
+                  onValueChange={this.onValueChange2.bind(this)}
+                >
+                  <Picker.Item label="Packaging Hall" value="key0" />
+                  <Picker.Item label="Main Auditorium" value="key1" />
+                  <Picker.Item label="Warehouse" value="key2" />
+                  <Picker.Item label="Machine Room" value="key3" />
+                  <Picker.Item label="Operations Office" value="key4" />
+                </Picker>
+              </Form>
+              <Button style={{ flex: 1}} onPress={() => this.props.navigation.navigate('submit')}>
+                      GO TO SUBMIT
+              </Button>
+            </ScrollView>
         </View>
     );  
   }
