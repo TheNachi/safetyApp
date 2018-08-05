06e508eaf6691e4b6d8c31fbd8af6979de6b9850
marquis-d-ugo <munachimso.ugorji@andela.com>
2018-08-05 04:36:57 +0100

    started creating content

---
+++ src/server/controllers/contents.js
@@ -0,0 +1,23 @@
+import db from '../models';
+
+export default {
+
+    create(req, res) {
+        db.Content.create(req.body)
+            .then((content) => {
+                res.status(201)
+                    .send(content);
+            })
+            .catch((err) => {
+                res.status(400)
+                    .send(err.errors);
+            })
+    },
+
+    list(req, res) {
+        db.Content.findAll()
+            .then((content) => {
+                res.send(content)
+            })
+    }
+}
