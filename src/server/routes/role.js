4f627a7de0b7def30b49b27b9b26b189440f8eeb
marquis-d-ugo <munachimso.ugorji@andela.com>
2018-08-21 22:04:18 +0100

    done with roles

--- src/server/routes/role.js
+++ src/server/routes/role.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import Roles from '../controllers/roles';
+import auth from '../middlewares/auth'
+
 
 const role = express.Router();
 
 role.route('/roles')
-    .post(Roles.create)
-    .get(Roles.list);
+    .post(auth.verifyToken, auth.permitAdmin, Roles.create)
+    .get(auth.verifyToken, auth.permitAdmin, Roles.list);
 
 role.route('/roles/:id')
-    .get(Roles.retrieve)
-    .put(Roles.update)
-    .delete(Roles.destroy);
+    .get(auth.verifyToken, auth.permitAdmin, Roles.retrieve)
+    .put(auth.verifyToken, auth.permitAdmin, Roles.update)
+    .delete(auth.verifyToken, auth.permitAdmin, Roles.destroy);
 
 export default role;
